689b8dba4f7efc4472ea008badc8d2ba664943be
Jason <jasonsoloriom@gmail.com>
2018-03-07 20:00:57 -0700

    (Maybe Temporary) page for adding-recipes

---
+++ src/pages/addrecipe/addrecipe.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-addrecipe',
+  templateUrl: 'addrecipe.html',
+})
+export class AddrecipePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+
+}
