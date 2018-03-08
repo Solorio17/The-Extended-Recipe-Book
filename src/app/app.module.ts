1386c874606858e0475be6f10df41524f0a1a3f8
Jason <jasonsoloriom@gmail.com>
2018-03-07 18:27:02 -0700

    Importing pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { ShoppinglistPage } from "../pages/shoppinglist/shoppinglist";
+import { RecipesPage } from "../pages/recipes/recipes"
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    TabsPage,
+    ShoppinglistPage,
+    RecipesPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    TabsPage,
+    ShoppinglistPage,
+    RecipesPage
   ],
   providers: [
     StatusBar,
