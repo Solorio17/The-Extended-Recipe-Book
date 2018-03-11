9ebbab9070bf2d53645c4e0cf3f4f5fd770068dc
Jason <jasonsoloriom@gmail.com>
2018-03-11 16:22:14 -0600

    Implementing functionality of selecting modes, creating a difficulty array, initializing the form, and creating the reactive form, that is used in edit-recipe.html

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,25 +1,33 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavParams } from 'ionic-angular';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
-/**
- * Generated class for the EditRecipePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-edit-recipe',
   templateUrl: 'edit-recipe.html',
 })
-export class EditRecipePage {
+export class EditRecipePage implements OnInit{
+  mode = "New";
+  selectOptions = ["Easy", "Medium", "Hard"];
+  recipeForm: FormGroup
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private navParams : NavParams){}
+
+  ngOnInit(){
+    this.mode = this.navParams.get('mode');
+    this.initializeForm();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EditRecipePage');
+  onsubmit(){
+    console.log(this.recipeForm)
   }
 
+  private initializeForm(){
+    this.recipeForm = new FormGroup({
+      'title': new FormControl(null, Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'difficulty': new FormControl("Medium", Validators.required)
+    });
+  }
 }
