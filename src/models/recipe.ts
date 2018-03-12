d037692a2d0bb48d8c584cc91a7f30845e253871
Jason <jasonsoloriom@gmail.com>
2018-03-11 20:53:04 -0600

    Creating a model for a recipe, to make sure every recipe has the same look and functionality

---
+++ src/models/recipe.ts
@@ -0,0 +1,9 @@
+import { Ingredient } from './ingredients';
+export class Recipe{
+  constructor(
+    public title: string,
+    public description: string,
+    public difficulty: string,
+    public ingredients: Ingredient[]
+  ){}
+}
