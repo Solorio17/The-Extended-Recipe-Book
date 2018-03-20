b443ded0775c4be0b8133c86b3cb05e2ce02292a
Jason <jasonsoloriom@gmail.com>
2018-03-20 10:43:46 -0600

    Not sure what was changed, but i believe that i had added that function to pull in the different ingredient values

--- src/pages/shoppinglist/shoppinglist.ts
+++ src/pages/shoppinglist/shoppinglist.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { NgForm } from "@angular/forms";
 import { ShoppingListService } from "../../services/shopping-list";
 import { Ingredient } from "../../models/ingredients"
-
 @Component({
   selector: 'page-shoppinglist',
   templateUrl: 'shoppinglist.html',
